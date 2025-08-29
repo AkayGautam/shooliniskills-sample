899953aeb933eef21d153dfa525c7838a3730956
arun gautam <akaygautam786@gmail.com>
2025-08-29 13:43:34 +0530

    replace the content

--- components/sections/Banner1.js
+++ components/sections/Banner1.js
@@ -9,13 +9,13 @@ export default function Banner1() {
                         <div className="col-lg-6">
                             <div className="banner__content">
                           
-                                <span className="sub-title" data-aos="fade-right" data-aos-delay={200}>100% Satisfaction
+                                <span className="sub-title">100% Satisfaction
                                     Guarantee</span>
-                                <h3 className="title tg-svg" data-aos="fade-right" data-aos-delay={400}>Make Acquiring  <span className="position-relative"><span className="svg-icon" id="svg-2" data-svg-icon="assets/img/icons/title_shape.svg" />Skills</span>Online Worthwhile</h3>
-                                <p data-aos="fade-right" data-aos-delay={600}>Career progression in 12–16 weeks with capstone and industry feedback.
+                                <h3 className="title tg-svg">Make Acquiring  <span className="position-relative"><span className="svg-icon" id="svg-2" data-svg-icon="assets/img/icons/title_shape.svg" />Skills</span>Online Worthwhile</h3>
+                                <p data-aos-delay={600}>Career progression in 12–16 weeks with capstone and industry feedback.
                                     Get certified by <strong> India’s No. 1 Private University - Shoolini University.</strong>
                                 </p>
-                                <div className="banner__btn-wrap" data-aos="fade-right" data-aos-delay={800}>
+                                <div className="banner__btn-wrap">
                                     <div className="tg-button-wrap">
                                         <Link href="/courses" className="btn tg-svg"><span className="text">Explore Courses</span>
                                             <span className="svg-icon" id="svg-1" data-svg-icon="assets/img/icons/btn-arrow.svg" /></Link>
@@ -32,9 +32,9 @@ export default function Banner1() {
                         </div>
                         <div className="col-lg-6">
                             <div className="banner__images">
-                                <img src="/assets/img/banner/bg-image.png" alt="img" className="main-img" />
+                                <img src="/assets/img/banner/banner_img1.png" alt="img" className="main-img" />
                                                               
-                                <div className="banner__fact">
+                                {/* <div className="banner__fact">
                                     <div className="banner__fact-item">
                                         <div className="icon">
                                             <i className="flaticon-group" />
@@ -53,7 +53,7 @@ export default function Banner1() {
                                             <h4 className="count">34K</h4>
                                         </div>
                                     </div>
-                                </div>
+                                </div> */}
                             </div>
                         </div>
                     </div>
